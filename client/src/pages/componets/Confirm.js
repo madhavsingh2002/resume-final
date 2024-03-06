d583c91bcb79132ca64a8f67240454815090565a
MADHAV.SINGH <madhav.singh@spraxa.com>
2024-03-06 19:33:51 +0530

    working on the image implemenation

--- client/src/pages/componets/Confirm.js
+++ client/src/pages/componets/Confirm.js
@@ -91,7 +91,7 @@ const Confirm = ({ formValues, setFormValues }) => {
         
         <Grid container spacing={2} lg={12} display="flex" >
         {formValues.image && (
-              <Grid item xs={12} sm={12} md={4} lg={4} sx={{ mt: "1",display:"flex",justifyContent:"flex-end",alignItems:"center",}}>
+              <Grid item xs={12} sm={6} md={4} lg={4} sx={{ mt: "1",display:"flex",justifyContent:{xs: "center",md: 0,lg: "flex-end",},alignItems:"center",}}>
                 <Box sx={{ width: 200, height: 200,marginTop:"14px" }}>
                   <Card>
                     <CardMedia
@@ -105,7 +105,7 @@ const Confirm = ({ formValues, setFormValues }) => {
               </Grid>
             )}
           {formValues.firstName && (
-            <Grid item xs={12} sm={12} md={8} lg={8} sx={{ mt: "1",display:"flex",alignItems:"center", justifyContent:"center"}}>
+            <Grid item xs={12} sm={6} md={8} lg={8} sx={{ mt: "1",display:"flex",alignItems:"center", justifyContent:"center"}}>
               <Typography variant="h4">
               {formValues.firstName} {formValues.lastName}
               </Typography>
